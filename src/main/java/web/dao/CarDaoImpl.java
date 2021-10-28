ec3d573e276a3eb35e78d6f0c5e307698e6b05d3
annaapraxina <91375621+annaapraxina@users.noreply.github.com>
2021-10-28 16:23:36 +0300

    Done

---
+++ src/main/java/web/dao/CarDaoImpl.java
@@ -0,0 +1,24 @@
+package web.dao;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Repository;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class CarDaoImpl implements CarDao{
+
+    @Override
+    public List<Car> getAllCars() {
+        List<Car> allCars = new ArrayList<>();
+        allCars.add(new Car("Lada", 456, "blue"));
+        allCars.add(new Car("Porsche", 888, "red"));
+        allCars.add(new Car("Tesla", 555, "black"));
+        allCars.add(new Car("Nissan", 246, "green"));
+        allCars.add(new Car("Reno", 729, "yellow"));
+        return allCars;
+    }
+}
+
